3ee6f118f11920f1be372b49fe39d510228c4286
Avishek Sen <x0rzavi@gmail.com>
2022-07-10 09:58:05 +0530

    ci: setup with script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env
+
+pacman-key --init
+pacman -Syyu --noconfirm
+
+pacman-key -r DDF7DB817396A49B2A2723F7403BD972F75D9D76
+pacman-key --lsign-key DDF7DB817396A49B2A2723F7403BD972F75D9D76
+cat >>/etc/pacman.conf << EOF
+
+[archzfs]
+Server = https://archzfs.com/$repo/$arch
+EOF
+
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+cat >>/etc/pacman.conf << EOF
+
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist 
+EOF
