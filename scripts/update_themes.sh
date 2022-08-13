33616355aa86bee52e7e95c340759dcff4eeb994
Avishek Sen <x0rzavi@gmail.com>
2022-08-13 17:51:45 +0530

    airootfs: update themes + scripts

--- scripts/update_themes.sh
+++ scripts/update_themes.sh
@@ -1,48 +1,55 @@
-#!/usr/bin/env bash
-#
-# Update theme files
+#!/bin/env dash
+
+# Description: Update theme files
+# Dependencies: sassc, gtk-engine-murrine, gnome-themes-extra
 
 # Safer script
+set -o errexit
+set -o nounset
 trap "exit" INT
-set -euo pipefail
 
 # Variables
 parent=$(pwd)
 
 # Cleanup
 cleanup () {
-    rm -fr $parent/Themes
-    rm -fr $parent/airootfs/etc/skel/.local/share/icons
-    rm -fr $parent/airootfs/etc/skel/.local/share/themes
-    echo -e "Cleanup done!"
+    rm -fr "${parent}"/Themes && \
+    rm -fr "${parent}"/airootfs/etc/skel/.local/share/icons && \
+    rm -fr "${parent}"/airootfs/etc/skel/.local/share/themes && \
+    rm -fr "${parent}"/airootfs/etc/skel/.config/gtk-4.0 && \
+    printf '%s\n' 'Cleanup done!'
 }
 
 # Mkdirs
 mkdirs () {
-    mkdir -p $parent/Themes
-    mkdir -p $parent/airootfs/etc/skel/.local/share/icons
-    mkdir -p $parent/airootfs/etc/skel/.local/share/themes
-    echo -e "Making dirs done!"
+    mkdir -p "${parent}"/Themes && \
+    mkdir -p "${parent}"/airootfs/etc/skel/.local/share/icons && \
+    mkdir -p "${parent}"/airootfs/etc/skel/.local/share/themes && \
+    printf '%s\n' 'Making dirs done!'
 }
 
 # Clone
 clone () {
-    git clone https://github.com/vinceliuice/Colloid-gtk-theme.git --depth=1 --quiet $parent/Themes/gtk
-    git clone https://github.com/vinceliuice/Fluent-icon-theme.git --depth=1 --quiet $parent/Themes/cursor
-    echo -e "Git cloning done!\n"
+    git clone https://github.com/vinceliuice/Colloid-gtk-theme.git --depth=1 --quiet "${parent}"/Themes/gtk && \
+    git clone https://github.com/vinceliuice/Colloid-icon-theme.git --depth=1 --quiet "${parent}"/Themes/cursor && \
+    printf '%s\n' 'Git cloning done!'
 }
 
 # Install GTK
 install_gtk () {
-    cd $parent/Themes/gtk && ./install.sh --tweaks rimless --dest $parent/airootfs/etc/skel/.local/share/themes
-    echo -e "\nGTK theme installed!\n"
+    cd "${parent}"/Themes/gtk && \
+    sed -i '630s|&& link_theme||' install.sh && \
+    ./install.sh --tweaks rimless --dest "${parent}"/airootfs/etc/skel/.local/share/themes && \
+    cp -r "${parent}"/airootfs/etc/skel/.local/share/themes/Colloid-Dark/gtk-4.0 "${parent}"/airootfs/etc/skel/.config/gtk-4.0 && \
+    printf '\n%s\n' 'GTK + libadwaita theme installed!'
 }
 
 # Install cursors
 install_cursors () {
-    /usr/bin/sed -i 's/DEST_DIR\=\"\$HOME\/\.local\/share\/icons\"/DEST_DIR\=\"$parent\/airootfs\/etc\/skel\/\.local\/share\/icons\"/' $parent/Themes/cursor/cursors/install.sh
-    cd $parent/Themes/cursor/cursors && source ./install.sh
-    echo -e "\nCursor theme installed!\n"
+    cd "${parent}"/Themes/cursor/cursors && \
+    sed -i "11s|\$HOME|${parent}/airootfs/etc/skel|" install.sh && \
+    ./install.sh && \
+    printf '\n%s\n' 'Cursor theme installed!'
 }
 
 cleanup
